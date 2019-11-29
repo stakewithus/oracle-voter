7b5264b00fd626d37c1cb2b4389c805cfc3eaa41
olwee <oliver@stakewith.us>
2019-11-29 09:44:53 +0000

    Saving StateMachine Refactoring

--- oracle-voter/oracle/machine2.py
+++ oracle-voter/oracle/machine2.py
@@ -1,5 +1,62 @@
+import asyncio
+
 from chain.core import FullNode, LCDNode, Transaction
 from wallet.cli import CLIWallet
+
+
+class State:
+    def __init__(self, name): self.name = name
+    def __str__(self): return self.name
+
+
+State.init = State("Init")
+State.ready = State("Ready")
+State.querying = State("Querying")
+State.pre_decision = State("PreDecision")
+State.voting = State("Voting")
+State.prevoting = State("PreVoting")
+State.broadcasting = State("Broadcasting")
+State.post_decision = State("PostDecision")
+
+
+class Input:
+    pass
+
+
+class NewVotingPeriod(Input):
+
+    def __init__(self, height, voting_period):
+        self.height = height
+        self.voting_period = voting_period
+
+
+class Condition:
+
+    @staticmethod
+    def test(self, input_data):
+        assert 0, "Abstract Method test() not implemented"
+
+
+class Transition:
+
+    @staticmethod
+    async def run(oracle_state, input_data):
+        assert 0, "Method run() not implemented"
+
+
+class Oracle:
+
+    def __init__(
+        self,
+        vote_period=1,
+        lcd_node=None,
+        validator_addr=None,
+        wallet=None,
+    ):
+        self.vote_period = 1
+        self.lcd_node = lcd_node
+        self.validator_addr = validator_addr
+        self.wallet = wallet
 
 
 async def main(
@@ -10,13 +67,29 @@ async def main(
     wallet_password="",
 ):
     lcd_node = LCDNode(lcd_node_addr)
-    feeder_wallet = CLIWallet(
+    # TODO Write Test Case to check for following failures
+    # - wallet_name is not found in the list of accounts
+    wallet = CLIWallet(
         wallet_name,
         wallet_password,
         lcd_node=lcd_node,
     )
     # Attempt to sync state from chain
     try:
-        await feeder_wallet.sync_state()
+        # TODO Write Test Case to check for following failures
+        # - feeder account has 0 account_number (not registered)
+        # - LCD Node cannot be reached
+        # - LCD Node returns non 20x HTTP Error
+
+        await wallet.sync_state()
+
+        oracle = Oracle(
+            vote_period=vote_period,
+            lcd_node=lcd_node,
+            validator_addr=validator_addr,
+            wallet=wallet,
+        )
+
+        oracle.start()
     except:
         pass
