a051e735796eb48fb0588ddf7533979ec49062b7
cryptobabybear <hello@oliverwee.com>
2019-12-09 04:18:38 +0000

    Improved test coverage to 92%, need to add assertions next

--- oracle-voter/chain/conftest.py
+++ oracle-voter/chain/conftest.py
@@ -6,17 +6,22 @@ from decimal import Decimal
 
 
 @pytest.fixture
-def feeder_wallet():
-    w = CLIWallet('oracle', '12345678')
-    return w
-
-
-@pytest.fixture
 def account_addrs():
     return (
         "terra1tngw4yusyas9ujlcmxdn7xkx6az07hej72rssm",  # Feeder
         "terravaloper1lsgzqmtyl99cxjs2rdrwvda3g6g6z8d3g8tfzu",  # Validator
     )
+
+
+@pytest.fixture
+def feeder_wallet(account_addrs):
+    feeder_addr = account_addrs[0]
+    w = CLIWallet(
+        'oracle',
+        '12345678',
+        feeder_addr,
+    )
+    return w
 
 
 @pytest.fixture
