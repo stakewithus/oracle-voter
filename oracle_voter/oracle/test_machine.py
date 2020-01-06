0113ad87f8dac10fcb0c93f27fb9b968bf1307b5
cryptobabybear <hello@oliverwee.com>
2020-01-06 17:14:20 +0800

    Refactoring tests

--- oracle_voter/oracle/test_machine.py
+++ oracle_voter/oracle/test_machine.py
@@ -2,6 +2,9 @@ import asyncio
 from asyncio import Future
 from unittest.mock import Mock, patch, MagicMock
 from oracle_voter.chain.core import LCDNode
+from oracle_voter.chain.fixtures_core import (
+    get_latest_block as stub_get_latest_block,
+)
 from oracle_voter.wallet.cli import CLIWallet
 from oracle_voter.config.test_settings import get_settings
 
@@ -9,156 +12,68 @@ from oracle_voter.oracle.machine2 import (
     Oracle,
 )
 
-from oracle_voter.chain.mocks.fixture_18549 import mock_height_18549
-from oracle_voter.chain.mocks.fixture_18550 import mock_height_18550
-from oracle_voter.chain.mocks.fixture_18555 import mock_height_18555
-from oracle_voter.chain.mocks.fixture_18559 import mock_height_18559
-from oracle_voter.chain.mocks.fixture_terracli import (
-    offline_sign_18549,
-    offline_sign_18550,
-    offline_sign_18555,
-)
-
 test_settings = get_settings()
 async def main_voting_e2e_3_periods(
     http_mock,
     LCDNodeMock,
+    CLIWalletMock,
     feed_coinone_url,
     feed_ukfx_url,
     vote_period,
 ):
     cli_accounts = (test_settings.get('validator_addr'), test_settings.get('feeder_addr'))
-    lcd_node = mock_height_18549(
-        http_mock,
-        feed_coinone_url,
-        feed_ukfx_url,
-        cli_accounts,
-        LCDNodeMock
-    )
-    feeder_wallet = CLIWallet(
-        test_settings.get('feeder_account'),
-        test_settings.get('feeder_pw'),
-        cli_accounts[1],
-        lcd_node,
-    )
-    offline_sign_18549(feeder_wallet)
-    #Fetch Initial State
-    await feeder_wallet.sync_state()
-    # Init the Start Machine
+    """LCDNodeMock is a Mocked LCDNode Instance with autospec
+    These are the function calls inside machine2.py that uses LCDNode
+        - get_tx(tx_hash)
+        - get_latest_block()
+        - get_oracle_rates()
+        - get_oracle_active_denoms()
+        - get_oracle_prevotes_validator(denom=denom, validator_addr=self.validator_addr)
+        - broadcast_tx_async(json.dumps({ tx: object, mode: "sync" }))
+    """
+
+    """CLIWalletMock is a Mocked CLIWallet Instance with autospec
+    These are the function calls inside machine2.py that uses wallet
+        - sync_wallet
+    Properties Accessed (Read-Only)
+        - account_num
+        - account_addr
+    Properties Mutated (Read-Write)
+        - account_seq
+    """
+
+    """
+    ---------------
+    | Block 18549 |
+    ---------------
+    """
+    blk = 18549
+    # get_latest_block
+    LCDNodeMock.get_latest_block.return_value = stub_get_latest_block(blk)
     oracle = Oracle(
         vote_period=vote_period,
-        lcd_node=lcd_node,
+        lcd_node=LCDNodeMock,
         validator_addr=cli_accounts[0],
-        wallet=feeder_wallet,
+        wallet=CLIWalletMock,
     )
-    # Mock the Salts
-    salt_mock = Mock()
-    salt_mock.side_effect = [
-        "bfaf",
-        "41a5",
-        "9d6e",
-        "28ef",
-    ]
-    oracle.get_rate_salt = salt_mock
-    # End Mock the Salts
     await oracle.retrieve_height()
-    #
-    # Mock Height 18550
-    #
-    lcd_node_188550 = mock_height_18550(
-        http_mock,
-        feed_coinone_url,
-        feed_ukfx_url,
-        cli_accounts,
-        LCDNodeMock,
-    )
-    oracle_188550 = Oracle(
-        vote_period=vote_period,
-        lcd_node=lcd_node_188550,
-        validator_addr=cli_accounts[0],
-        wallet=feeder_wallet,
-    )
-    # Mock the Salts
-    salt_mock = Mock()
-    salt_mock.side_effect = [
-        "ff2e",
-        "2d3c",
-        "1e47",
-        "d534",
-    ]
-    oracle_188550.get_rate_salt = salt_mock
-    offline_sign_18550(feeder_wallet)
-    await oracle_188550.retrieve_height()
-    #
-    # Mock Height 18555
-    #
-    lcd_node_188555 = mock_height_18555(
-        http_mock,
-        feed_coinone_url,
-        feed_ukfx_url,
-        cli_accounts,
-        LCDNodeMock,
-    )
-    oracle_188555 = Oracle(
-        vote_period=vote_period,
-        lcd_node=lcd_node_188555,
-        validator_addr=cli_accounts[0],
-        wallet=feeder_wallet,
-    )
-    # Mock the Salts
-    salt_mock = Mock()
-    salt_mock.side_effect = [
-        "3595",
-        "39ff",
-        "4721",
-        "0c26",
-    ]
-    oracle_188555.get_rate_salt = salt_mock
-    offline_sign_18555(feeder_wallet)
-    await oracle_188555.retrieve_height()
-    #
-    # Mock Height 18559
-    #
-    oracle_18559 = mock_height_18559(
-        http_mock,
-        feed_coinone_url,
-        feed_ukfx_url,
-        cli_accounts,
-        LCDNodeMock,
-    )
-    oracle_188555 = Oracle(
-        vote_period=vote_period,
-        lcd_node=oracle_18559,
-        validator_addr=cli_accounts[0],
-        wallet=feeder_wallet,
-    )
-    await oracle_188555.retrieve_height()
-
+    
 
 @patch('oracle_voter.chain.core.LCDNode', autospec=True)
+@patch('oracle_voter.wallet.cli.CLIWallet', autospec=True)
 def test_voting_e2e_3_periods(
+    CLIWalletMock,
     LCDNodeMock,
     http_mock,
     feed_coinone_url,
     feed_ukfx_url,
 ):
-    # Mock all required endpoints
-    """
-    voting_e2e_3_periods(
-        http_mock,
-        node_addr,
-        feed_coinone_url,
-        feed_ukfx_url,
-        cli_accounts,
-        feeder_wallet,
-    )
-    """
-
     # Start Loop
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main_voting_e2e_3_periods(
         http_mock,
         LCDNodeMock,
+        CLIWalletMock,
         feed_coinone_url,
         feed_ukfx_url,
         5,  # Vote Period
