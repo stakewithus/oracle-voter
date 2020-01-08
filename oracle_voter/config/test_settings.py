4cb64edddcd6687a9f859017cb22c69b8f88bfb3
TanDN <dntan@stakewith.us>
2020-01-08 09:22:40 +0700

    fix chain tests

--- oracle_voter/config/test_settings.py
+++ oracle_voter/config/test_settings.py
@@ -7,6 +7,7 @@ def get_settings():
         'feeder_addr': 'terra1pwm7nz0nt7kz45rm0x7jq0qhm6sl4t0ukpvk3y',
         'validator_addr': 'terravaloper1rhrptnx87ufpv62c7ngt9yqlz2hr77xr9nkcr9',
         'oracle_addr': 'terra1tngw4yusyas9ujlcmxdn7xkx6az07hej72rssm',
+        'oracle_validator_addr': 'terravaloper1lsgzqmtyl99cxjs2rdrwvda3g6g6z8d3g8tfzu',
         'chain_test_public_key': 'AsyXH0ftWQ29WxzgwpfV2WJ7glylgPnaOPdcAfPQ+Fyk',
         'chain_test_signature': 'ZOI24iYEoW4GmMCIaFvoCjSoBO1fZyuryaOwjaNavzYAjK9ebgs1PLkD6hhlZ7umIRCvLhNTZkspoEwKM1w/UQ==',
 
@@ -18,6 +19,7 @@ def get_settings():
         # 'feeder_addr': 'terra1s68kcs88pcgpavg24wzgxwtklyvg7p6k6cshwj',
         # 'validator_addr': 'terravaloper1kk2gcmy6d444jpsg3hyf84lxd3dx0naudqxv26',
         # 'oracle_addr': 'terra1sl2anjsenxjg4ewh98hjx0qtc0m3ap3976dm5f',
+        # 'oracle_validator_addr': 'terravaloper1kk2gcmy6d444jpsg3hyf84lxd3dx0naudqxv26',
         # 'chain_test_public_key': 'A/MpdCdPzvhn0yDp+3bNIiV51qD/K1g7S2U1P9ZQH4dB',
         # 'chain_test_signature': '6xHICXGGdFn4lJkEpxlSDdNq70PO6zg8pBWSRk73cFJqyeOt0Stj+wFhiZTZ1u4xesi/xZ1AVYBEBaXkGdOPFQ=='
     }
